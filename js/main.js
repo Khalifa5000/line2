517c061af197ac510c0d242413a80198aa5d7607
Mohamed Khalifa <106628515+Khalifa5000@users.noreply.github.com>
2025-09-12 01:08:10 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,3 +2,8 @@ let item = document.querySelector("h1");
 item.onclick = function(){
 item.style.color= "green";
 }
+
+let btn = document.queryselector("button");
+btn.onclick = ()=>{
+btn.style.color= "blue";
+}
